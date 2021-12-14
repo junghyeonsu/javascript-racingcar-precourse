88a595166d408a8f1f9a7404449990173bb1cb35
정현수 <jung660317@naver.com>
2021-12-14 19:06:26 +0900

    chore: Selector 상수 파일 생성
    - SELECTOR 변수를 상수 파일에 생성하여 id값들을 넣어둠

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SELECTOR = Object.freeze({
+  carNamesInputId: 'car-names-input',
+  carNamesSubmitId: 'car-names-submit',
+  racingCountInputId: 'racing-count-input',
+  racingCountSubmitId: 'racing-count-submit',
+  racingWinnersId: 'racing-winners',
+});
